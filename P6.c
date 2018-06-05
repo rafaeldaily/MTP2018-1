6c7a13022e9fa7fc25b84769e4f4f43e6922231c
Rafael Daily <38022239+rafaeldaily@users.noreply.github.com>
2018-06-05 10:30:43 -0300

    P6

---
+++ P6.c
@@ -0,0 +1,41 @@
+//Nome: Rafael Daily Santos Martins
+//Matrícula: 11721EEL001
+#include <stdio.h>
+
+int soma(int quant, int vet[])
+{
+
+	if(quant == 0)
+		return 0;
+	else
+	{	
+		int s;
+    	s = soma (quant-1, vet);
+    	if (vet[quant-1] > 0) 
+			s = s + vet[quant-1];
+    	return (s);
+   }
+}
+float media(int quant, int soma)
+{
+	return soma/quant;
+}
+int main()
+{
+	int num=0, vet[20], i, somatorio;
+	for(i=0; i<20; i++)
+		vet[i]=0;
+	do
+	{
+		printf("Digite a quantidade de numeros: ");
+		scanf("%d", &num);
+	}while(num<5 && num>20);
+	for(i=0; i<num; i++)
+	{
+		printf("\nDigite um numero: ");
+		scanf("%d", &vet[i]);
+	}
+	somatorio = soma(num, vet);
+	printf("Somatorio: %d\nMedia: %f", somatorio, media(num, somatorio));
+	return 0;
+}
